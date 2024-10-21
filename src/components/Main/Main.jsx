1c596136e6ae9520786f638bf64ac47719228376
igaurav11 <kg15navodayan@gmail.com>
2024-10-22 04:39:23 +0530

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+import { useContext } from "react";
+import "./Main.css";
+import { Context } from "../../Context/Contex";
+
+const Main = () => {
+    const {
+        onSent,
+        recentPrompt,
+        showResults,
+        loading,
+        resultData,
+        setInput,
+        input,
+    } = useContext(Context);
+
+    const handleCardClick = (promptText) => {
+        setInput(promptText);
+    };
+    return (
+        <div className="main">
+            <div className="nav">
+                <p>Gemini</p>
+                <img src='images/boy.png' alt="" />
+            </div>
+            <div className="main-container">
+                {!showResults ? (
+                    <>
+                        <div className="greet">
+                            <p>
+                                <span>Hello , Dev </span>
+                            </p>
+                            <p>How Can i Help You Today?</p>
+                        </div>
+                        <div className="cards">
+                            <div
+                                className="card"
+                                onClick={() =>
+                                    handleCardClick("Suggest Some Place To Visit In India.")
+                                }
+                            >
+                                <p>Suggest Some Place To Visit In India.</p>
+                                <img src='images/compass.png' alt="" />
+                            </div>
+                            <div
+                                className="card"
+                                onClick={() =>
+                                    handleCardClick(
+                                        "Explain the process of photosynthesis in simple terms"
+                                    )
+                                }
+                            >
+                                <p>Explain the process of photosynthesis in simple terms </p>
+                                <img src='images/compass.png' alt="" />
+                            </div>
+                            <div
+                                className="card"
+                                onClick={() =>
+                                    handleCardClick("How do you create a responsive navbar using CSS and JavaScript?")
+                                }
+                            >
+                                <p>How do you create a responsive navbar using CSS and JavaScript?</p>
+                                <img src='images/bulb.png' alt="" />
+                            </div>
+                            <div
+                                className="card"
+                                onClick={() => {
+                                    handleCardClick(
+                                        "What are some essential skills for becoming a front-end developer?"
+                                    );
+                                }}
+                            >
+                                <p>What are some essential skills for becoming a front-end developer?</p>
+                                <img src='images/code.png' alt="" />
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <div className="result">
+                        <div className="result-title">
+                            <img src='images/boy.png' alt="" />
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                            <img src='images/icon.png' alt="" />
+                            {loading ? (
+                                <div className="loader">
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                </div>
+                            ) : (
+                                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                            )}
+                        </div>
+                    </div>
+                )}
+
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input
+                            onChange={(e) => {
+                                setInput(e.target.value);
+                            }}
+                            value={input}
+                            type="text"
+                            placeholder="Enter the Prompt Here"
+                        />
+                        <div>
+                            <img src='images/gallery.png' alt="" />
+                            <img src='images/mic.png' alt="" />
+                            <img
+                                src='images/send_icon.png'
+                                alt=""
+                                onClick={() => {
+                                    onSent();
+                                }}
+                            />
+                        </div>
+                    </div>
+                    <div className="bottom-info">
+                        <p>
+                            Gemini may display inaccurate info, including about people, so
+                            double-check its responses. Your privacy & Gemini Apps
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,64 @@
+import "./sidebar.css";
+import { useContext, useState } from "react";
+import { Context } from "../../Context/Contex";
+
+const Sidebar = () => {
+	const [extended, setExtended] = useState(false);
+	const { onSent, prevPrompts, setRecentPrompt, newChat } = useContext(Context);
+
+	const loadPreviousPrompt = async (prompt) => {
+		setRecentPrompt(prompt);
+		await onSent(prompt);
+	};
+	return (
+		<div className="sidebar">
+			<div className="top">
+				<img
+					src='images/menu.png'
+					className="menu"
+					alt="menu-icon"
+					onClick={() => {
+						setExtended((prev) => !prev);
+					}}
+				/>
+				<div className="new-chat">
+					<img src='images/plus.png' alt="" onClick={()=>{
+                        newChat()
+                    }} />
+					{extended ? <p>New Chat</p> : null}
+				</div>
+				{extended ? (
+					<div className="recent">
+						<p className="recent-title">Recent</p>
+						{prevPrompts.map((item, index) => {
+							return (
+								<div onClick={()=>{
+                                    loadPreviousPrompt(item)
+                                }} className="recent-entry">
+									<img src='images/message.png' alt="" />
+									<p>{item.slice(0, 18)}...</p>
+								</div>
+							);
+						})}
+					</div>
+				) : null}
+			</div>
+			<div className="bottom">
+				<div className="bottom-item recent-entry">
+					<img src='images/question.png' alt="" />
+					{extended ? <p>Help desk</p> : null}
+				</div>
+				<div className="bottom-item recent-entry">
+					<img src='images/history.png' alt="" />
+					{extended ? <p>History</p> : null}
+				</div>
+				<div className="bottom-item recent-entry">
+					<img src='images/settings.png' alt="" />
+					{extended ? <p>Settings</p> : null}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
